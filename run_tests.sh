b343ac3130b5028fa61154393425e161e748c62f
Felix Rabe <public@felixrabe.net>
2009-11-03 15:17:06 +0100

    Small changes to the testing framework

--- run_tests.sh
+++ run_tests.sh
@@ -13,7 +13,7 @@ cmd=( "$PY" convert_newlines.py )
 echo "${cmd[@]}"
 "${cmd[@]}"
 
-for f in tests/*.in.xml
+for f in tests/${1}*.in.xml
 do
     base=${f%.in.xml}
     cmd=( "$PY" xmlmerge.py -i "$f" -o "$base.out.xml" -r "$base.ref.xml" -d )
